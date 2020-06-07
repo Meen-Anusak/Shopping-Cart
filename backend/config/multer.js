aded7d537112ab3f1acc9a545b194ed944845a81
MeenAnusak <50317541+Meen-Anusak@users.noreply.github.com>
2020-06-07 23:26:44 +0700

    Update multer.js

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -27,13 +27,3 @@ module.exports = MulterConfig = {
 }
 
 
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, '/tmp/my-uploads')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-})
-
-var upload = multer({ storage: storage })
